cb8cb97cde1b25012f605e3d305b38ef48cb340a
dine-j <dine.j.neko@gmail.com>
2017-04-06 15:14:58 +0400

    Move classes to src package

---
+++ src/CheckOut.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutSystem
+{
+    class CheckOut
+    {
+        Dictionary<Item, int> basket;
+        double totalPrice;
+
+        CheckOut()
+        {
+            basket = new Dictionary<Item, int>();
+            totalPrice = 0;
+        }
+
+        void scan(Item item)
+        {
+            if(basket.ContainsKey(item))
+            {
+                ++basket[item];
+            }
+            else
+            {
+                basket.Add(item, 1);
+            }
+        }
+
+        void remove(Item item)
+        {
+
+        }
+
+        double total()
+        {
+            return totalPrice;
+        }
+    }
+}
---
+++ src/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutSystem
+{
+    class Item : IEquatable<Item>
+    {
+        String name;
+        double price;
+        int thresholdDiscount;
+        double discountedPrice;
+
+        public Item(String name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public Item(String name, double price, int thresholdDiscount, double discountedPrice)
+        {
+            this.name = name;
+            this.price = price;
+            this.thresholdDiscount = thresholdDiscount;
+            this.discountedPrice = discountedPrice;
+        }
+
+        public bool Equals(Item other)
+        {
+            return !ReferenceEquals(null, other) && name.Equals(other.name);
+        }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode();
+        }
+    }
+}
