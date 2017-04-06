6ba43bd4878ba32b322f6acd17c908c4073edf01
dine-j <dine.j.neko@gmail.com>
2017-04-06 19:17:47 +0400

    Add comments

--- CheckOutSystem/src/Item.cs
+++ CheckOutSystem/src/Item.cs
@@ -9,12 +9,24 @@ namespace CheckoutSystem
         int thresholdDiscount;
         double discountedPrice;
 
+        /// <summary>
+        /// Constructor for item priced individually
+        /// </summary>
+        /// <param name="name">Name of the item</param>
+        /// <param name="price">Price of the item</param>
         public Item(String name, double price)
         {
             this.name = name;
             this.price = price;
         }
 
+        /// <summary>
+        /// Constructor for multi-priced item
+        /// </summary>
+        /// <param name="name">Name of the item</param>
+        /// <param name="price">Price of the item</param>
+        /// <param name="thresholdDiscount">Number of required item to activate the discount</param>
+        /// <param name="discountedPrice">Price for given number of items</param>
         public Item(String name, double price, int thresholdDiscount, double discountedPrice)
         {
             this.name = name;
@@ -23,6 +35,11 @@ namespace CheckoutSystem
             this.discountedPrice = discountedPrice;
         }
 
+        /// <summary>
+        /// Get total price for this item given a quantity
+        /// </summary>
+        /// <param name="quantity">Quantity needed</param>
+        /// <returns>Total price for the item</returns>
         public double GetTotal(int quantity)
         {
             if(thresholdDiscount != 0)
